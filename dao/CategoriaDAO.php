76ee2a12ff6b93b1de2cf62fd5b212ce32012c25
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-09-14 23:52:15 -0500

    feat: Creacion del DAO para categoria

--- dao/CategoriaDAO.php
+++ dao/CategoriaDAO.php
@@ -1,11 +1,21 @@
 <?php
+require_once __DIR__. "/../model/Categoria.php";
 
 class CategoriaDAO{
 
-    private $con
+    private $conn;
 
-    public function __construct(){
-        $this->conn = $db
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function registrar(Categoria $categoria){
+        $sql = "INSERT INTO categoria (nombre_categoria) VALUES (:nombre_categoria)";
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bindValue(":nombre_categoria", $categoria->getNombreCategoria());
+        return $stmt->execute();
+
     }
 
 
